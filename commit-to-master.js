641e8d6fd7a8491a62dd3bc8b1845fea9c01bde6
Tom Manion <tom.manion@autodesk.com>
2015-10-15 13:48:22 -0500

    test

---
+++ commit-to-master.js
@@ -0,0 +1,10 @@
+
+function runCommand(cmd, args, cb) {
+    var spawn = require('child_process').spawn,
+        child = spawn(cmd, args),
+        me = this;
+    child.stdout.on('data', function (buffer) { cb(me, buffer) });
+    child.stdout.on('end', function() {
+    	cb(data);
+    });
+}
