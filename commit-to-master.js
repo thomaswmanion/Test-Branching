10ad5aea61ebb0f1fd488d15e8344b9c8ebc8c48
Tom Manion <tom.manion@autodesk.com>
2015-10-15 13:50:13 -0500

    Antoher test

--- commit-to-master.js
+++ commit-to-master.js
@@ -1,10 +1,1 @@
-
-function runCommand(cmd, args, cb) {
-    var spawn = require('child_process').spawn,
-        child = spawn(cmd, args),
-        me = this;
-    child.stdout.on('data', function (buffer) { cb(me, buffer) });
-    child.stdout.on('end', function() {
-    	cb(data);
-    });
-}
+var a = 'test';
